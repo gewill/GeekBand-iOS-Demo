b2f57902609a94012096e501de725a995f27edba
gewill <531sunlight@gmail.com>
2015-07-29 19:31:18 +0800

    add BLDemo03

---
+++ iOS APP Development/BLDemo03/BLDemo03/BLOneViewController.h
@@ -0,0 +1,14 @@
+//
+//  BLOneViewController.h
+//  BLDemo03
+//
+//  Created by Will Ge on 7/29/15.
+//  Copyright (c) 2015 gewill.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BLBaseViewController.h"
+
+@interface BLOneViewController : BLBaseViewController
+
+@end
---
+++ iOS APP Development/BLDemo03/BLDemo03/BLOneViewController.m
@@ -0,0 +1,34 @@
+//
+//  BLOneViewController.m
+//  BLDemo03
+//
+//  Created by Will Ge on 7/29/15.
+//  Copyright (c) 2015 gewill.org. All rights reserved.
+//
+
+#import "BLOneViewController.h"
+
+
+@interface BLOneViewController ()
+
+@end
+
+@implementation BLOneViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"one";
+    
+    // 未自定义 iamge，显示 base 默认背景图片
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+
+
+@end
